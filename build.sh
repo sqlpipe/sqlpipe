89a88144afdba08efc0a0cc06eb53acc68675e3f
Cal Mitchell <calmitchell617@gmail.com>
2022-02-02 17:41:14 +0000

    working on build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 current_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 git_description=$(git describe --always --dirty --tags --long)
 
-GOOS=linux GOARCH=arm64 go build -ldflags=${linker_flags} -o=./bin/linux/sqlpipe ./cmd
-GOOS=darwin GOARCH=arm64 go build -ldflags=${linker_flags} -o=./bin/darwin/sqlpipe ./cmd
+GOOS=linux GOARCH=arm64 go build -ldflags='-s -X main.gitHash=${git_description}' -o=./bin/linux/sqlpipe ./cmd
+GOOS=darwin GOARCH=arm64 go build -ldflags='-s -X main.gitHash=${git_description}' -o=./bin/darwin/sqlpipe ./cmd
 GOOS=windows GOARCH=arm64 go build -ldflags='-s -X main.gitHash=${git_description}' -o=./bin/windows/sqlpipe ./cmd
