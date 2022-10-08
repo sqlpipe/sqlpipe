f238f0d1c75457d9e86ef527cfea11658d2c88e3
Ubuntu <ubuntu@ip-172-31-32-92.eu-west-3.compute.internal>
2022-10-08 06:03:28 +0000

    changed api version to v2

--- cmd/sqlpipe/routes.go
+++ cmd/sqlpipe/routes.go
@@ -13,13 +13,13 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v2/healthcheck", app.healthcheckHandler)
 
-	router.HandlerFunc(http.MethodPost, "/v1/query", app.authenticate(app.runQueryHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/transfer", app.authenticate(app.runTransferHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/csv/download", app.authenticate(app.runCsvDownloadHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/csv/s3", app.authenticate(app.runCsvS3UploadHandler))
-	router.HandlerFunc(http.MethodPost, "/v1/csv/save", app.authenticate(app.runCsvSaveOnServerHandler))
+	router.HandlerFunc(http.MethodPost, "/v2/query", app.authenticate(app.runQueryHandler))
+	router.HandlerFunc(http.MethodPost, "/v2/transfer", app.authenticate(app.runTransferHandler))
+	router.HandlerFunc(http.MethodPost, "/v2/csv/download", app.authenticate(app.runCsvDownloadHandler))
+	router.HandlerFunc(http.MethodPost, "/v2/csv/s3", app.authenticate(app.runCsvS3UploadHandler))
+	router.HandlerFunc(http.MethodPost, "/v2/csv/save", app.authenticate(app.runCsvSaveOnServerHandler))
 
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
